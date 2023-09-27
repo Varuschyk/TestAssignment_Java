de793ae9b2955d826d23714dc96695666cc1b67d
AlexVar <lalkawtf@gmail.com>
2023-09-27 17:24:30 +0300

    initial commit

---
+++ src/main/java/com/alexvar/testassignment_java/TestAssignmentJavaApplication.java
@@ -0,0 +1,13 @@
+package com.alexvar.testassignment_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssignmentJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestAssignmentJavaApplication.class, args);
+  }
+
+}
